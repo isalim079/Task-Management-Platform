e9e182b0704cc7e2f770fb300cff649ae832b634
isalim079 <salimreza1025@gmail.com>
2023-12-22 01:38:55 +0600

    Benefited table added

--- src/pages/Home/Benefit/BenifitTable.jsx
+++ src/pages/Home/Benefit/BenifitTable.jsx
@@ -1,3 +1,6 @@
+import Rating from "react-rating";
+import { MdOutlineStarBorder, MdOutlineStarPurple500 } from "react-icons/md";
+
 /* eslint-disable react/prop-types */
 const BenifitTable = ({ benefitUser }) => {
     // console.log(benefitUser);
@@ -7,14 +10,22 @@ const BenifitTable = ({ benefitUser }) => {
 
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+            <div className="card-body flex justify-center items-center">
+                <h2 className="card-title">{name}</h2>
+                <p className="text-sm">{profession}</p>
+                <div className="border-2 border-teal-800 w-full "></div>
+                <p className="text-center text-base">{feedback}</p>
+                <div className="card-actions justify-center">
+                    <Rating
+                        className="text-3xl text-teal-800"
+                        initialRating={review}
+                        stop={5}
+                        emptySymbol={<MdOutlineStarBorder />}
+                        fullSymbol={<MdOutlineStarPurple500 />}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
     );
 };
 
