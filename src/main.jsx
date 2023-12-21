d4392ad2ef78133e64735b19c1e09ba2650585f2
isalim079 <salimreza1025@gmail.com>
2023-12-21 13:43:32 +0600

    basic router setup page done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/Router";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <div>
+        <RouterProvider router={router}></RouterProvider>
+        </div>
+    </React.StrictMode>
+);
