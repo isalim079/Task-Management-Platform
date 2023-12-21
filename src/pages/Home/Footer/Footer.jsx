3ccd7986945fc8056a8021ea8ad75cb2e6f83dbf
isalim079 <salimreza1025@gmail.com>
2023-12-22 02:02:41 +0600

    footer secion added

---
+++ src/pages/Home/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <div>
+            <div className="bg-teal-800 p-10">
+                <div className="max-w-screen-xl mx-auto text-white">
+                    <div className="text-xl md:text-3xl font-medium text-white flex justify-between items-center"><span className="border border-white h-0 w-[520px]"></span> TASKER <span className="border border-white h-0 w-[520px]"></span></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
