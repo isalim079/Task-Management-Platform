662bd1d956cf1cdd84b9ecf75bbcadd8e6d5d0b8
isalim079 <salimreza1025@gmail.com>
2023-12-25 13:05:40 +0600

    provate route added

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,6 +1,10 @@
+/* eslint-disable react/prop-types */
 import { useContext } from "react";
 import { AuthContext } from "./AuthProvider";
-import { useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
+
+import loadingAnimation from "../assets/lottieAnimation/loading.json"
+import Lottie from "lottie-react";
 
 
 const PrivateRoute = ({children}) => {
@@ -8,11 +12,13 @@ const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext)
     const location = useLocation()
 
-    return (
-        <div>
-            
-        </div>
-    );
+   if (loading) {
+    return <div><Lottie animationData={loadingAnimation} loop={true} /></div>
+   }
+   if(user) {
+    return children
+   }
+   return <Navigate state={location?.pathname} to="/login"></Navigate>
 };
 
 export default PrivateRoute;
