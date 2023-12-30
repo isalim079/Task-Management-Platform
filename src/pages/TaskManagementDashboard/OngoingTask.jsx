2a062765d024563384418948d0462b8255a45d21
isalim079 <salimreza1025@gmail.com>
2023-12-30 22:23:37 +0600

    ongoing task draggable successfully added

--- src/pages/TaskManagementDashboard/OngoingTask.jsx
+++ src/pages/TaskManagementDashboard/OngoingTask.jsx
@@ -1,27 +1,42 @@
+import { Draggable } from "react-beautiful-dnd";
+
 /* eslint-disable react/prop-types */
 const OngoingTask = ({ task, index }) => {
     // console.log(task);
 
     return (
-        <div className="grid grid-cols-8 ">
-            <div className="mb-4 mt-4 bg-teal-100 p-3 border-l-4 border-teal-900 col-span-7">
-                <h1>
-                    <span className="font-semibold">Task Name:</span>{" "}
-                    {task?.taskName}
-                </h1>
-                <p>
-                    <span className="font-semibold">Description:</span>{" "}
-                    {task?.taskDescription}
-                </p>
-                <p className="capitalize">
-                    <span className="font-semibold">Priority:</span>{" "}
-                    {task?.taskPriority}
-                </p>
-            </div>
-            <div className="col-span-1 mt-4 bg-teal-100 mb-4 flex items-center justify-center">
-                <p>#{index + 1}</p>
-            </div>
-        </div>
+        <Draggable
+            key={task?._id}
+            draggableId={(index + 1).toString()}
+            index={index + 1}
+        >
+            {(provided) => (
+                <div
+                    ref={provided.innerRef}
+                    {...provided.dragHandleProps}
+                    {...provided.draggableProps}
+                    className="grid grid-cols-8 "
+                >
+                    <div className="mb-4 mt-4 bg-teal-100 p-3 border-l-4 border-teal-900 col-span-7">
+                        <h1>
+                            <span className="font-semibold">Task Name:</span>{" "}
+                            {task?.taskName}
+                        </h1>
+                        <p>
+                            <span className="font-semibold">Description:</span>{" "}
+                            {task?.taskDescription}
+                        </p>
+                        <p className="capitalize">
+                            <span className="font-semibold">Priority:</span>{" "}
+                            {task?.taskPriority}
+                        </p>
+                    </div>
+                    <div className="col-span-1 mt-4 bg-teal-100 mb-4 flex items-center justify-center">
+                        <p>#{index + 1}</p>
+                    </div>
+                </div>
+            )}
+        </Draggable>
     );
 };
 
--- src/pages/TaskManagementDashboard/TaskDemo.jsx
+++ src/pages/TaskManagementDashboard/TaskDemo.jsx
@@ -3,6 +3,7 @@ import useAxiosPublic from "../../hooks/useAxiosPublic";
 import CompletedTask from "./CompletedTask";
 import OngoingTask from "./OngoingTask";
 import TaskNav from "./TaskNav";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 const TaskDemo = () => {
     const axiosPublic = useAxiosPublic();
@@ -26,41 +27,76 @@ const TaskDemo = () => {
         });
     }, [axiosPublic]);
 
+    const handleDragDrop = (result) => {
+        console.log(result);
+    };
+
     return (
-        <div className="grid grid-cols-8 h-screen gap-10">
-            <div>
-                <TaskNav />
-            </div>
-            {/* Task Container */}
-            <div className="col-span-7">
-                <div className="grid grid-cols-5 gap-5 mt-10 mr-10">
-                    {/* Ongoing task */}
-                    <div className="col-span-3 border h-96 overflow-auto p-4 border-teal-600">
-                        <div>
-                            <h1 className="text-center text-2xl font-semibold uppercase">
-                                Ongoing
-                            </h1>
-                            {/* Underline */}
-                            <div className="flex justify-center">
-                                <div className="border-2 border-teal-900 w-52"></div>
+        <DragDropContext onDragEnd={handleDragDrop}>
+            <div className="grid grid-cols-8 h-screen gap-10">
+                <div>
+                    <TaskNav />
+                </div>
+                {/* Task Container */}
+                <div className="col-span-7">
+                    <div className="grid grid-cols-5 gap-5 mt-10 mr-10">
+                        {/* Ongoing task */}
+                        <Droppable droppableId="ongoing">
+                           {(provided) => (
+                             <div {...provided.droppableProps} ref={provided.innerRef} className="col-span-3 border h-96 overflow-auto p-4 border-teal-600">
+                             <div>
+                                 <h1 className="text-center text-2xl font-semibold uppercase">
+                                     Ongoing
+                                 </h1>
+                                 {/* Underline */}
+                                 <div className="flex justify-center">
+                                     <div className="border-2 border-teal-900 w-52"></div>
+                                 </div>
+                                 {/* -------------- */}
+                             </div>
+                             {incompleteTask?.map((task, index) => (
+                                 
+                                    
+                                        <OngoingTask
+                                        key={task?._id}
+                                        task={task}
+                                        index={index}
+                                    />
+                                    
+                                 
+                             ))}
+                              {provided.placeholder}
+                         </div>
+                           )}
+                        </Droppable>
+                        {/* Completed task */}
+                        <Droppable droppableId="completed" >
+                            {(provided) => (
+                                <div {...provided.droppableProps} ref={provided.innerRef} className="col-span-2 border h-96 overflow-auto p-4 border-teal-600 ">
+                                <div>
+                                    <h1 className="text-center text-2xl font-semibold uppercase">
+                                        Completed
+                                    </h1>
+                                    {/* Underline */}
+                                    <div className="flex justify-center">
+                                        <div className="border-2 border-teal-900 w-52"></div>
+                                    </div>
+                                    {/* -------------- */}
+                                </div>
+                                {completedTask?.map((task, index) => (
+                                    <CompletedTask
+                                        key={task?._id}
+                                        task={task}
+                                        index={index}
+                                    />
+                                ))}
                             </div>
-                            {/* -------------- */}
-                        </div>
-                        {completedTask?.map((task, index) => (
-                            <OngoingTask
-                                key={task?._id}
-                                task={task}
-                                index={index}
-                            />
-                        ))}
-                    </div>
-                    {/* Completed task */}
-                    <div className="col-span-2 border h-96 overflow-auto p-4 border-teal-600 ">
-                        <CompletedTask />
+                            )}
+                        </Droppable>
                     </div>
                 </div>
             </div>
-        </div>
+        </DragDropContext>
     );
 };
 
